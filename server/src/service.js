0b0d008fc57e6497e9c9ae770833eb5146c63531
ML6503 <ml6503@gmail.com>
2023-02-04 16:26:25 +0200

    feat: add routes

--- server/src/service.js
+++ server/src/service.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
 
-module.exports = async (_req, res) => {
+module.exports = async (req, res) => {
   // TODO  arrange  if or switch for req.url
   //case `/offers`
   // case `/airports`
   // case `/flights`
+  console.log('we are in service');
   fs.readFile(__dirname + '/backend_xml_responce.xml', 'utf8', (err, data) => {
     if (err) {
       res.statusCode = 500;
@@ -13,18 +14,9 @@ module.exports = async (_req, res) => {
       throw err;
     }
 
-    const options = {
-      valueProcessors: [
-        xml2js.processors.parseNumbers,
-        xml2js.processors.firstCharLowerCase,
-        xml2js.processors.parseBooleans,
-      ],
-    };
-
     xml2js.parseString(
       data,
-      // { firstCharLowerCase: true, parseNumbers: true, parseBooleans: true },
-      // options,
+
       {
         attrNameProcessors: [
           xml2js.processors.firstCharLowerCase,
@@ -39,12 +31,36 @@ module.exports = async (_req, res) => {
           throw err;
         }
         console.log('OFFERS: ', result.SearchResult.Offers[0].Item[0]);
-        // console.log('OFFERS: ', result.SearchResult.Offers.Item);
+
         const offers = result.SearchResult.Offers[0].Item;
-        // const offers = result.SearchResult.Offers.Item;
+        const airports = result.SearchResult.References[0].Airports[0].Item;
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.end(JSON.stringify(offers));
+
+        switch (req.url) {
+          case '/offers':
+            res.writeHead(200);
+            res.end(JSON.stringify(offers));
+            break;
+          case '/airports':
+            res.writeHead(200);
+            // res.end('airports');
+            res.end(JSON.stringify(airports));
+            break;
+          case '/flights':
+            res.writeHead(200);
+            res.end('flights');
+            // res.end(JSON.stringify(flights));
+            break;
+          case '/':
+            res.writeHead(200);
+            res.end(JSON.stringify({ message: 'Server is running' }));
+            // res.end(JSON.stringify(flights));
+            break;
+          default:
+            res.writeHead(404);
+            res.end(JSON.stringify({ error: 'Resource not found' }));
+        }
       }
     );
   });
